f3a9f7a67adf3c5507eb038ee1f33212a9c8589a
Franko <167652997+francocerda@users.noreply.github.com>
2024-08-08 09:04:39 -0400

    añadir listado 1 ejercicios

---
+++ Algoco-INF221/Listado_1/b.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define int long long 
+#define vi vector<int>
+#define USM ios::sync_with_stdio(0); cin.tie(0);
+const int mod = 1e9+7;
+
+using namespace std;
+
+signed main() {
+    USM;
+
+    int n;cin>>n;
+    vi arr;
+    arr.resize(n);
+    for(int i=0;i<n;i++){
+        cin >> arr[i];
+    }
+
+
+
+    return 0;
+
+}    
