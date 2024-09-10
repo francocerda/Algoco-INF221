9f37250978e2f6a3e555b7e2d78744f0463e3b5c
Franko <franco.cerda@sansano.usm.cl>
2024-09-10 01:49:53 -0300

    e  py

---
+++ Listados/Listado_2/d.py
@@ -0,0 +1,21 @@
+s = input()
+split_s = s.split()
+
+m = int(input())
+
+for i in range(m):
+    song = input()
+    list_song = []
+    list_song.insert(i,song)
+    m -= 1
+
+n = int(input())
+for i in range(n):
+    sort = input()
+    list_sort = []
+    list_sort.insert(i,sort)
+    n -= 1
+
+
+
+
