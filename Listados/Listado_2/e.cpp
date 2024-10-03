712b24768e3ff3c5b3ef3bc16b407ffa04dc2db0
Franco Cerda <frankoadvance20@gmail.com>
2024-10-02 23:54:26 -0300

    a

--- Listados/Listado_2/e.cpp
+++ Listados/Listado_2/e.cpp
@@ -10,36 +10,50 @@ const int mod = 1e9+7;
 
 using namespace std;
 
-signed main() {
-    USM;
+int N;
+int A[101], B[101];
+vector<pair<int,int> > arr[2];
 
-    int n;cin>>n;
-    set<int> arr_A,arr_B;
-    for(int i=0;i<n;i++){
-        int a,b;cin>> a>>b;
-        arr_A.insert(a);arr_B.insert(b);
-        auto A_inicio = arr_A.begin();
-        auto B_inicio = arr_B.begin();
-        auto A_fin = arr_A.end();
-        auto B_fin = arr_B.end();
-        if(*(A_fin--) <= *(B_fin--)){
-            if(*A_inicio >= *B_inicio){
-                cout << *A_inicio+*(B_fin--) << endl;
-                
-                
-            }
-            else{
-                cout << *B_inicio+*(A_fin--) << endl;
-            
+signed main(){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    cin >> N;
+    for (int i = 0; i < N; ++i) {
+        int a, b;
+        cin >> a >> b;
+        A[a]++;
+        B[b]++;
+        for (int j = 1; j <= 100; ++j) {
+            if (A[j] > 0)
+            arr[0].push_back({j, A[j]});
+        }
+        for (int j = 100; j >= 1; --j) {
+            if (B[j] > 0)
+            arr[1].push_back({j, B[j]});
+        }
+        int idx[] = {0, 0};
+        int sz[] = {(int)arr[0].size(), (int)arr[1].size()};
+        int ans = -1;
+        while (idx[0] < sz[0] && idx[1] < sz[1]) {
+            pair<int,int> bil1 = arr[0][idx[0]];
+            pair<int,int> bil2 = arr[1][idx[1]];
+            if (bil1.second == bil2.second) {
+                idx[0]++;
+                idx[1]++;
+                ans = max(ans, bil1.first + bil2.first);
+            } else if (bil1.second > bil2.second) {
+                idx[1]++;
+                ans = max(ans, bil1.first + bil2.first);
+                arr[0][idx[0]].second -= bil2.second;
+            } else {
+                idx[0]++;
+                ans = max(ans, bil1.first + bil2.first);
+                arr[1][idx[1]].second -= bil1.second;
             }
         }
-        else{
-
-            cout << *(B_fin--) + *A_inicio << endl;
-
+        cout << ans << '\n';
+        for (int j = 0; j < 2; ++j) {
+            arr[j].clear();
         }
     }
-        
-    
     return 0;
 }
